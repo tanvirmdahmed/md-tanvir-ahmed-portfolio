f90df6bb7b3b10bcff04242d6eb7b6947d0427f0
tanvirmdahmed <developertanvirahmed09@gmail.com>
2023-06-27 16:05:01 +0600

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    "react"
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+    "react/no-unescaped-entities": 0,
+    "react/prop-types": "off",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error"
+  },
+}
