c9f9dc0980c2fdb67269e9f7851d702fbe84abe6
tanvirmdahmed <developertanvirahmed09@gmail.com>
2023-06-28 10:04:45 +0600

    home section has completed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './About.css'
+import Info from '../../components/Info';
+import { FaDownload } from 'react-icons/fa';
+import Resume from '../../assets/wd-resume-of-tanvir.pdf'
+import Stats from '../../components/Stats';
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <main className="section container">
+            <section className="about">
+                <h2 className="section__title">
+                    About <span>Me</span>
+                </h2>
+
+                <div className="about__container grid">
+                    <div className="about__info">
+                        <h3 className="section__subtitle">Personal Infos</h3>
+
+                        <ul className="info__list grid">
+                            <Info></Info>
+                        </ul>
+
+                        <a href={Resume} download='' className="button">
+                            Download Resume <span className="button__icon"><FaDownload></FaDownload></span>
+                        </a>
+                    </div>
+
+                    <div className="stats grid">
+                        <Stats></Stats>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css'
 import Profile from '../../assets/home.jpg'
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Home = () => {
     return (
@@ -9,8 +11,25 @@ const Home = () => {
 
             <div className="home__content">
                 <div className="home__data">
-                    h1
+                    <h1 className="home__title">
+                        <span>I'm Md. Tanvir Ahmed.</span> MERN Stack Web Developer
+                    </h1>
+
+                    <p className="home__description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequatur commodi, voluptatibus aliquid ratione earum, aspernatur nostrum nihil ipsam numquam nam blanditiis cupiditate. Voluptate, eius
+                    </p>
+
+                    <Link to='/about' className='button'>
+                        More About Me{' '}
+                        <span className='button__icon'>
+                            <FaArrowRight></FaArrowRight>
+                        </span>
+                    </Link>
                 </div>
+            </div>
+
+            <div className="color__block">
+
             </div>
         </section>
     );
